6a7a54d9bdf53f4f5dcbab81e2a53d70f1ce10cb
GregPhi <gregoire.philippot@laoste.net>
2021-01-22 12:57:33 +0100

    create : quiz fragment setup empty fragment setup toolbar & filters menu

---
+++ ZooDeLille/app/src/main/java/com/example/zoodelille/QuizFragment.java
@@ -0,0 +1,55 @@
+package com.example.zoodelille;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toolbar;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class QuizFragment extends Fragment {
+    private static final String name = "Quiz";
+    private View m_view;
+    private Toolbar toolbar;
+
+    public static QuizFragment newInstance(){
+        return new QuizFragment();
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        super.onCreateView(inflater, container, savedInstanceState);
+        m_view = inflater.inflate(R.layout.fragment_quizzes, container, false);
+        return m_view;
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        toolbar = (Toolbar) m_view.findViewById(R.id.toolbar_quizzes);
+        toolbar.inflateMenu(R.menu.quizzes_filter);
+        toolbar.setTitle(R.string.quizzes);
+        toolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                if(item.getItemId()==R.id.quizzes_filter_alpha)
+                {
+                    // do something
+                }
+                else if(item.getItemId()== R.id.quizzes_filter_make)
+                {
+                    // do something
+                }
+                else{
+                    // do something
+                }
+                return false;
+            }
+        });
+    }
+}
