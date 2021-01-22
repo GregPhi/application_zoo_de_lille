600db366b6bbb2079a1aa75b8c0b63b0b1c90d60
GregPhi <gregoire.philippot@laoste.net>
2021-01-22 12:07:38 +0100

    fix : main activity setup of the BottomNavigation element fragment display setup

--- ZooDeLille/app/src/main/java/com/example/zoodelille/MainActivity.java
+++ ZooDeLille/app/src/main/java/com/example/zoodelille/MainActivity.java
@@ -1,14 +1,82 @@
 package com.example.zoodelille;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Context;
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 public class MainActivity extends AppCompatActivity {
+    private BottomNavigationView m_BottomNav;
+    public static final List<Fragment> m_listFragment = new ArrayList<Fragment>() {{
+        add(HomeFragment.newInstance());
+        add(RouteFragment.newInstance());
+        add(AnimalFragment.newInstance());
+        add(MapFragment.newInstance());
+        add(QuizFragment.newInstance());
+        add(QRCodeFragment.newInstance());
+    }};
+    private static final int positionHomeFragment = 0;
+    private static final int positionRouteFragment = 1;
+    private static final int positionAnimalFragment = 2;
+    private static final int positionMapFragment = 3;
+    private static final int positionQuizFragment = 4;
+    private static final int positionQRCodeFragment = 5;
+    public int m_currentFragment = positionHomeFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        m_BottomNav = findViewById(R.id.bottom_navigation);
+        m_BottomNav.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.nav_home:
+                        m_currentFragment = positionHomeFragment;
+                        break;
+                    case R.id.nav_routes:
+                        m_currentFragment = positionRouteFragment;
+                        break;
+                    case R.id.nav_animals:
+                        m_currentFragment = positionAnimalFragment;
+                        break;
+                    case R.id.nav_map:
+                        m_currentFragment = positionMapFragment;
+                        break;
+                    case R.id.nav_quizzes:
+                        m_currentFragment = positionQuizFragment;
+                        break;
+                    /*case R.id.nav_qrcode:
+                        m_currentFragment = positionQRCodeFragment;
+                        break;*/
+                }
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, m_listFragment.get(m_currentFragment)).commit();
+                return true;
+            }
+        });
+
+        if(savedInstanceState != null) {
+            m_currentFragment = savedInstanceState.getInt("currentPositionFragment");
+        }
+        else {
+            m_currentFragment = positionHomeFragment;
+        }
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                m_listFragment.get(m_currentFragment)).commit();
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle bundle) {
+        super.onSaveInstanceState(bundle);
+        bundle.putInt("currentPositionFragment", m_currentFragment);
     }
 }
