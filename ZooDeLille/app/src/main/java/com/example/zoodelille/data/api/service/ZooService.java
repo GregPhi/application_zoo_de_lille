c4b6e4fb621df5a48bb0dd2017eea51b85112849
GregPhi <gregoire.philippot@laoste.net>
2021-01-29 00:19:52 +0100

    create : service use to get information from API

---
+++ ZooDeLille/app/src/main/java/com/example/zoodelille/data/api/service/ZooService.java
@@ -0,0 +1,23 @@
+package com.example.zoodelille.data.api.service;
+
+import com.example.zoodelille.data.api.object.Zoo;
+import com.example.zoodelille.data.api.object.animal.Animal;
+
+import java.util.List;
+
+import io.reactivex.Single;
+import retrofit2.http.GET;
+import retrofit2.http.Path;
+
+public interface ZooService {
+    String URL = Local.URL+"zoo-api/";
+
+    @GET("version")
+    Single<Zoo> getVersion();
+
+    @GET("animals/")
+    Single<List<Animal>> getAllAnimals();
+
+    @GET("animals/{id}")
+    Single<Animal> getAnAnimalWithId(@Path("id") int id);
+}
