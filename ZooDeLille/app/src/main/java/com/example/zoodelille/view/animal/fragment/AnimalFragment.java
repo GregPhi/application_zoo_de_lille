6796e61fc3b91b6a666eddbedbb5fb30ec1c9e5e
GregPhi <gregoire.philippot@laoste.net>
2021-01-29 00:37:34 +0100

    fix : animal fragment display animal retrieve from API setup user action

--- ZooDeLille/app/src/main/java/com/example/zoodelille/view/animal/fragment/AnimalFragment.java
+++ ZooDeLille/app/src/main/java/com/example/zoodelille/view/animal/fragment/AnimalFragment.java
@@ -1,5 +1,6 @@
 package com.example.zoodelille.view.animal.fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.MenuItem;
@@ -8,15 +9,29 @@ import android.view.ViewGroup;
 import android.widget.Toolbar;
 
 import com.example.zoodelille.R;
+import com.example.zoodelille.data.di.DepencyInjector;
+import com.example.zoodelille.view.animal.adapter.Action;
+import com.example.zoodelille.view.animal.adapter.AnimalItemViewModel;
+import com.example.zoodelille.view.animal.adapter.AnimalListAdapter;
+import com.example.zoodelille.view.animal.info.AnimalInfoActivity;
+import com.example.zoodelille.view.model.AnimalViewModel;
+
+import java.util.List;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
-public class AnimalFragment extends Fragment {
+public class AnimalFragment extends Fragment implements Action {
     private static final String name = "Animal";
     private View m_view;
     private Toolbar toolbar;
+    private AnimalViewModel animalViewModel;
+    private AnimalListAdapter animalListAdapter;
 
     public static AnimalFragment newInstance(){
         return new AnimalFragment();
@@ -33,7 +48,9 @@ public class AnimalFragment extends Fragment {
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
-        toolbar = (Toolbar) m_view.findViewById(R.id.toolbar_animals);
+        setupRecyclerView();
+        initRecyclerView();
+        toolbar = m_view.findViewById(R.id.toolbar_animals);
         toolbar.inflateMenu(R.menu.animals_filter);
         toolbar.setTitle(R.string.animals);
         toolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
@@ -53,5 +70,41 @@ public class AnimalFragment extends Fragment {
                 return false;
             }
         });
+    }
+
+    public void setupRecyclerView(){
+        RecyclerView recyclerView = m_view.findViewById(R.id.recyclerview_animals);
+        animalListAdapter = new AnimalListAdapter(this);
+        recyclerView.setAdapter(animalListAdapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+
+    }
+
+    public void initRecyclerView(){
+        animalViewModel = new ViewModelProvider(requireActivity(), DepencyInjector.getViewModelFactoryAnimal()).get(AnimalViewModel.class);
+        animalViewModel.getAnimals().observe(getViewLifecycleOwner(), new Observer<List<AnimalItemViewModel>>() {
+            @Override
+            public void onChanged(List<AnimalItemViewModel> animalItemViewModels) {
+                animalListAdapter.setAnimals(animalItemViewModels);
+            }
+        });
+    }
+
+    public void setupRecyclerViewWithAlphaFilter(){}
+
+    public void setupRecyclerViewWithLikeFilter(){}
+
+    public void setupRecyclerViewWithAlimFilter(){}
+
+    @Override
+    public void changeStatutOfFavorite(int id) {
+
+    }
+
+    @Override
+    public void displayAnimal(AnimalItemViewModel animal) {
+        Intent intent = new Intent(getContext(), AnimalInfoActivity.class);
+        intent.putExtra("animal",animal);
+        startActivity(intent);
     }
 }
