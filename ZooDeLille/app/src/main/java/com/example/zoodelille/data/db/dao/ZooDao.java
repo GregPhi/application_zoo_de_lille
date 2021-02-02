78ad9b895bdb76d977cea3ce4f4a1112ca45f474
GregPhi <gregoire.philippot@laoste.net>
2021-02-02 11:42:38 +0100

    fix : update return type of findZooVersion() now return a Signle<List<ZooEntity>>

--- ZooDeLille/app/src/main/java/com/example/zoodelille/data/db/dao/ZooDao.java
+++ ZooDeLille/app/src/main/java/com/example/zoodelille/data/db/dao/ZooDao.java
@@ -2,9 +2,10 @@ package com.example.zoodelille.data.db.dao;
 
 import com.example.zoodelille.data.entity.ZooEntity;
 
+import java.util.List;
+
 import androidx.room.Dao;
 import androidx.room.Insert;
-import androidx.room.OnConflictStrategy;
 import androidx.room.Query;
 import androidx.room.Update;
 import io.reactivex.Completable;
@@ -12,12 +13,13 @@ import io.reactivex.Single;
 
 @Dao
 public interface ZooDao {
-    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    @Insert
     Completable insert(ZooEntity zooEntity);
 
-    @Update(onConflict = OnConflictStrategy.REPLACE)
+    @Update
     Completable update(ZooEntity zooEntity);
 
-    @Query("SELECT * FROM zooentity WHERE id = 0")
-    Single<ZooEntity> findZooVersion();
+
+    @Query("SELECT * FROM zooentity")
+    Single<List<ZooEntity>> findZooVersion();
 }
