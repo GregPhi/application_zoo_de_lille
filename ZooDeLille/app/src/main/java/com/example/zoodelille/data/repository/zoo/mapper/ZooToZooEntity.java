920265a5ccfda5897993d4d7868b325029b27ae2
GregPhi <gregoire.philippot@laoste.net>
2021-02-02 12:30:32 +0100

    fix : transfom a Zoo object to Zoo entity now using Date

--- ZooDeLille/app/src/main/java/com/example/zoodelille/data/repository/zoo/mapper/ZooToZooEntity.java
+++ ZooDeLille/app/src/main/java/com/example/zoodelille/data/repository/zoo/mapper/ZooToZooEntity.java
@@ -7,22 +7,7 @@ public class ZooToZooEntity {
     public ZooEntity map(Zoo zoo){
         ZooEntity zooEntity = new ZooEntity();
         zooEntity.setId(zoo.getId());
-        zooEntity.setAnimal_version(zoo.getAnimal_version());
-        zooEntity.setInfo_version(zoo.getInfo_version());
-        zooEntity.setInterest_version(zoo.getInterest_version());
-        zooEntity.setRoute_version(zoo.getRoute_version());
-        zooEntity.setQuiz_version(zoo.getQuiz_version());
+        zooEntity.setDate_last_version(zoo.getDate_last_version());
         return zooEntity;
-    }
-
-    public Zoo reverseMap(ZooEntity zooEntity){
-        Zoo zoo = new Zoo();
-        zoo.setId(zooEntity.getId());
-        zoo.setAnimal_version(zooEntity.getAnimal_version());
-        zoo.setInfo_version(zooEntity.getInfo_version());
-        zoo.setInterest_version(zooEntity.getInterest_version());
-        zoo.setRoute_version(zooEntity.getRoute_version());
-        zoo.setQuiz_version(zooEntity.getQuiz_version());
-        return zoo;
     }
 }
