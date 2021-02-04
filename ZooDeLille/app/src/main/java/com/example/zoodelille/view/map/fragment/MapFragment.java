7ad04b29f9a4c84713a0ef2f105df3734e3c05b5
GregPhi <gregoire.philippot@laoste.net>
2021-02-04 19:04:28 +0100

    fix : import

--- ZooDeLille/app/src/main/java/com/example/zoodelille/view/map/fragment/MapFragment.java
+++ ZooDeLille/app/src/main/java/com/example/zoodelille/view/map/fragment/MapFragment.java
@@ -1,5 +1,6 @@
 package com.example.zoodelille.view.map.fragment;
 
+import com.example.zoodelille.R;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapView;
@@ -8,7 +9,6 @@ import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.GroundOverlay;
 import com.google.android.gms.maps.model.GroundOverlayOptions;
 import com.google.android.gms.maps.model.LatLng;
-import com.example.zoodelille.R;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
