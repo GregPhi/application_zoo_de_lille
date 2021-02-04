392c466c5776d745d0ddb2f80b83bbf4dd5f793a
GregPhi <gregoire.philippot@laoste.net>
2021-02-04 17:28:04 +0100

    fix : project add type converters

--- ZooDeLille/app/src/main/java/com/example/zoodelille/data/db/ProjectDatabase.java
+++ ZooDeLille/app/src/main/java/com/example/zoodelille/data/db/ProjectDatabase.java
@@ -2,13 +2,16 @@ package com.example.zoodelille.data.db;
 
 import com.example.zoodelille.data.db.dao.AnimalDao;
 import com.example.zoodelille.data.db.dao.ZooDao;
+import com.example.zoodelille.data.db.typeconverters.Converters;
 import com.example.zoodelille.data.entity.ZooEntity;
 import com.example.zoodelille.data.entity.animal.AnimalEntity;
 
 import androidx.room.Database;
 import androidx.room.RoomDatabase;
+import androidx.room.TypeConverters;
 
 @Database(entities = {AnimalEntity.class, ZooEntity.class}, version=1,exportSchema = false)
+@TypeConverters({Converters.class})
 public abstract class ProjectDatabase extends RoomDatabase {
     public abstract AnimalDao animalDao();
     public abstract ZooDao zooDao();
