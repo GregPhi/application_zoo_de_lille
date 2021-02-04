5c6dcc81b9527a564853019183468186b173a227
GregPhi <gregoire.philippot@laoste.net>
2021-02-04 17:28:57 +0100

    fix : update method to retrieve animal entity add method to retrieve entity with different criteria

--- ZooDeLille/app/src/main/java/com/example/zoodelille/data/db/dao/AnimalDao.java
+++ ZooDeLille/app/src/main/java/com/example/zoodelille/data/db/dao/AnimalDao.java
@@ -23,9 +23,20 @@ public interface AnimalDao {
     @Query("DELETE FROM animalentity WHERE id = :id")
     Completable deleteAnimalWithId(int id);
 
+    @Query("SELECT * FROM animalentity WHERE id = :id")
+    Single<AnimalEntity> getAnimalEntity(int id);
+
     @Query("SELECT * FROM animalentity")
     Flowable<List<AnimalEntity>> getAllAnimal();
 
-    @Query("SELECT * FROM animalentity WHERE id = :id")
-    Single<AnimalEntity> getAnimalEntity(int id);
+    @Query("SELECT * FROM animalentity ORDER BY " +
+            "CASE WHEN :isAsc = 1 THEN name END ASC," +
+            "CASE WHEN :isAsc = 0 THEN name END DESC")
+    Flowable<List<AnimalEntity>> getAllAnimalOnAZ_or_ZA(boolean isAsc);
+
+    @Query("SELECT * FROM animalentity WHERE favorite = :isFavorite")
+    Flowable<List<AnimalEntity>> getAllAnimalIsFavorite_or_Not(boolean isFavorite);
+
+    @Query("SELECT id FROM animalentity WHERE favorite = 1")
+    Single<List<Integer>> getAllFavoriteAnimalId();
 }
