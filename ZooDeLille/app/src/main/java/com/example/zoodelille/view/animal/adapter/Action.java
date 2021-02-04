78326936e78f79b6db95612455913f9866c53352
GregPhi <gregoire.philippot@laoste.net>
2021-02-04 17:37:24 +0100

    fix : mathod Action

--- ZooDeLille/app/src/main/java/com/example/zoodelille/view/animal/adapter/Action.java
+++ ZooDeLille/app/src/main/java/com/example/zoodelille/view/animal/adapter/Action.java
@@ -1,7 +1,7 @@
 package com.example.zoodelille.view.animal.adapter;
 
 public interface Action {
-    void changeStatutOfFavorite(int id);
+    void changeStatutOfFavorite(AnimalItemViewModel animalItem);
 
     void displayAnimal(AnimalItemViewModel animal);
 }
