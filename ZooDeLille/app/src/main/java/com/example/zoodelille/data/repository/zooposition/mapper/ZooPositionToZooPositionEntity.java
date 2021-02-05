85f77616bc0873d5d40a57d1ffabecffe2a26e5c
GregPhi <gregoire.philippot@laoste.net>
2021-02-05 01:05:02 +0100

    fix : update definition of the map method

--- ZooDeLille/app/src/main/java/com/example/zoodelille/data/repository/zooposition/mapper/ZooPositionToZooPositionEntity.java
+++ ZooDeLille/app/src/main/java/com/example/zoodelille/data/repository/zooposition/mapper/ZooPositionToZooPositionEntity.java
@@ -3,23 +3,12 @@ package com.example.zoodelille.data.repository.zooposition.mapper;
 import com.example.zoodelille.data.api.object.ZooPosition;
 import com.example.zoodelille.data.entity.ZooPositionEntity;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class ZooPositionToZooPositionEntity {
-    public ZooPositionEntity map(ZooPosition zooPosition){
+    public static ZooPositionEntity map(ZooPosition zooPosition){
         ZooPositionEntity zooPositionEntity = new ZooPositionEntity();
         zooPositionEntity.setZooPosition_id(zooPosition.getId());
         zooPositionEntity.setLatitude(zooPosition.getLatitude());
         zooPositionEntity.setLongitude(zooPosition.getLongitude());
         return zooPositionEntity;
-    }
-
-    public List<ZooPositionEntity> map(List<ZooPosition> zooPositionList){
-        List<ZooPositionEntity> zooPositionEntities = new ArrayList<>();
-        for(ZooPosition zooPosition : zooPositionList){
-            zooPositionEntities.add(map(zooPosition));
-        }
-        return zooPositionEntities;
     }
 }
