7f0a637a065e4db98419cf6008561a2ae2bfdc81
GregPhi <gregoire.philippot@laoste.net>
2021-02-05 01:01:28 +0100

    fix : add InfoDao and InfoEntity to class

--- ZooDeLille/app/src/main/java/com/example/zoodelille/data/db/ProjectDatabase.java
+++ ZooDeLille/app/src/main/java/com/example/zoodelille/data/db/ProjectDatabase.java
@@ -1,18 +1,21 @@
 package com.example.zoodelille.data.db;
 
 import com.example.zoodelille.data.db.dao.AnimalDao;
+import com.example.zoodelille.data.db.dao.InfoDao;
 import com.example.zoodelille.data.db.dao.ZooDao;
 import com.example.zoodelille.data.db.typeconverters.Converters;
 import com.example.zoodelille.data.entity.ZooEntity;
 import com.example.zoodelille.data.entity.animal.AnimalEntity;
+import com.example.zoodelille.data.entity.info.InfoEntity;
 
 import androidx.room.Database;
 import androidx.room.RoomDatabase;
 import androidx.room.TypeConverters;
 
-@Database(entities = {AnimalEntity.class, ZooEntity.class}, version=1,exportSchema = false)
+@Database(entities = {AnimalEntity.class, ZooEntity.class, InfoEntity.class}, version=1,exportSchema = false)
 @TypeConverters({Converters.class})
 public abstract class ProjectDatabase extends RoomDatabase {
     public abstract AnimalDao animalDao();
     public abstract ZooDao zooDao();
+    public abstract InfoDao infoDao();
 }
