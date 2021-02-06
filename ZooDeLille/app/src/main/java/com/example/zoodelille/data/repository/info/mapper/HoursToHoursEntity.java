1619709b812926c01a59621b871991128937df98
GregPhi <gregoire.philippot@laoste.net>
2021-02-06 20:35:01 +0100

    fix : mapper to set correctly the Date with formater

--- ZooDeLille/app/src/main/java/com/example/zoodelille/data/repository/info/mapper/HoursToHoursEntity.java
+++ ZooDeLille/app/src/main/java/com/example/zoodelille/data/repository/info/mapper/HoursToHoursEntity.java
@@ -1,23 +1,31 @@
 package com.example.zoodelille.data.repository.info.mapper;
 
+import android.text.TextUtils;
+
 import com.example.zoodelille.data.api.object.info.hours.Hours;
 import com.example.zoodelille.data.entity.info.hours.HoursEntity;
 
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 public class HoursToHoursEntity {
     public static HoursEntity map(Hours hours){
         SimpleDateFormat formater;
-        formater = new SimpleDateFormat("'le' dd MMMM yyyy");
+        formater = new SimpleDateFormat("dd MMMM yyyy");
         HoursEntity hoursEntity = new HoursEntity();
         hoursEntity.setSummerEntity(SummerToSummerEntity.map(hours.getSummer()));
         hoursEntity.setWinterEntity(WinterToWinterEntity.map(hours.getWinter()));
-        String exceptional_opening = "";
-        for(Date date : hours.getExceptional_opening()){
-            exceptional_opening += formater.format(date)+"\n";
+        if(hours.getExceptional_opening() == null){
+            hoursEntity.setExceptional_opening("Non Communiqué");
+        }else{
+            List<String> strings = new ArrayList<>();
+            for(Date date : hours.getExceptional_opening()){
+                strings.add(formater.format(date));
+            }
+            hoursEntity.setExceptional_opening(TextUtils.join("\n", strings));
         }
-        hoursEntity.setExceptional_opening(exceptional_opening);
         hoursEntity.setAnnual_closure_oldYear(formater.format(hours.getAnnual_closure_oldYear()));
         hoursEntity.setAnnual_closure_newYear(formater.format(hours.getAnnual_closure_newYear()));
         hoursEntity.setAttention(hours.getAttention());
