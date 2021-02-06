cf4b1dfebc76ff0adc872219f987d521ae16b3e6
GregPhi <gregoire.philippot@laoste.net>
2021-02-06 20:30:05 +0100

    fix : getInfoEntity request update return type of this method to work correctly

--- ZooDeLille/app/src/main/java/com/example/zoodelille/data/db/dao/InfoDao.java
+++ ZooDeLille/app/src/main/java/com/example/zoodelille/data/db/dao/InfoDao.java
@@ -4,12 +4,14 @@ import com.example.zoodelille.data.entity.info.InfoEntity;
 
 import java.util.List;
 
+import androidx.room.Dao;
 import androidx.room.Insert;
 import androidx.room.OnConflictStrategy;
 import androidx.room.Query;
 import io.reactivex.Completable;
-import io.reactivex.Single;
+import io.reactivex.Flowable;
 
+@Dao
 public interface InfoDao {
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     Completable addInfo(InfoEntity infoEntity);
@@ -18,5 +20,5 @@ public interface InfoDao {
     Completable deleteInfoWithId(int id);
 
     @Query("SELECT * FROM infoentity")
-    Single<List<InfoEntity>> getInfoEntity();
+    Flowable<List<InfoEntity>> getInfoEntity();
 }
