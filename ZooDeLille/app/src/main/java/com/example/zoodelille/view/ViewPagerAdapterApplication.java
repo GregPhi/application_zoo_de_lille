56206606dae42951e468b32ffaf3712506eba17d
GregPhi <gregoire.philippot@laposte.net>
2021-02-08 12:11:43 +0100

    create : view page adaper adapter which allows you to create the links between the application fragments

---
+++ ZooDeLille/app/src/main/java/com/example/zoodelille/view/ViewPagerAdapterApplication.java
@@ -0,0 +1,49 @@
+package com.example.zoodelille.view;
+
+import com.example.zoodelille.view.animal.fragment.AnimalFragment;
+import com.example.zoodelille.view.home.fragment.HomeFragment;
+import com.example.zoodelille.view.map.fragment.MapFragment;
+import com.example.zoodelille.view.qrcode.fragment.QRCodeFragment;
+import com.example.zoodelille.view.quiz.fragment.QuizFragment;
+import com.example.zoodelille.view.route.fragment.RouteFragment;
+
+import org.jetbrains.annotations.NotNull;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.lifecycle.Lifecycle;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+
+public class ViewPagerAdapterApplication extends FragmentStateAdapter {
+
+    public ViewPagerAdapterApplication(@NonNull FragmentManager fragmentManager, @NonNull Lifecycle lifecycle) {
+        super(fragmentManager, lifecycle);
+    }
+
+    @NotNull
+    @Override
+    public Fragment createFragment(int position) {
+        if(position == 1){
+            return RouteFragment.newInstance();
+        }
+        if(position == 2){
+            return AnimalFragment.newInstance();
+        }
+        if(position == 3){
+            return MapFragment.newInstance();
+        }
+        if(position == 4){
+            return QuizFragment.newInstance();
+        }
+        if(position == 5){
+            return QRCodeFragment.newInstance();
+        }
+        return HomeFragment.newInstance();
+    }
+
+    @Override
+    public int getItemCount() {
+        return 6;
+    }
+}
