f718850fba39ce8801db814754aed070a2c30096
GregPhi <gregoire.philippot@laposte.net>
2021-02-08 21:41:00 +0100

    create : dao to make action on the database -> answer dao => make request on answer entity -> question dao => make request on question entity -> quiz dao => make request on quiz entity

---
+++ ZooDeLille/app/src/main/java/com/example/zoodelille/data/db/dao/AnswerDao.java
@@ -0,0 +1,31 @@
+package com.example.zoodelille.data.db.dao;
+
+import com.example.zoodelille.data.entity.quiz.answer.AnswerEntity;
+
+import java.util.List;
+
+import androidx.room.Dao;
+import androidx.room.Insert;
+import androidx.room.OnConflictStrategy;
+import androidx.room.Query;
+import io.reactivex.Completable;
+import io.reactivex.Flowable;
+import io.reactivex.Single;
+
+@Dao
+public interface AnswerDao {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    Completable addAnswer(AnswerEntity answerEntity);
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    Completable addAllAnswer(List<AnswerEntity> answerEntity);
+
+    @Query("DELETE FROM answerEntity WHERE answer_id = :id")
+    Completable deleteAnswerWithId(int id);
+
+    @Query("SELECT * FROM answerEntity WHERE answer_id = :id")
+    Single<AnswerEntity> getAnswerEntity(int id);
+
+    @Query("SELECT * FROM AnswerEntity")
+    Flowable<List<AnswerEntity>> getAllAnswer();
+}
