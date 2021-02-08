868d697b60dbd6549d51ee94fda80982b8232838
GregPhi <gregoire.philippot@laposte.net>
2021-02-09 00:01:00 +0100

    create ; management of answer entity retrieve answer entity from database map answer object to answer entity

---
+++ ZooDeLille/app/src/main/java/com/example/zoodelille/data/repository/quiz/answer/local/AnswerLocalDataSource.java
@@ -0,0 +1,42 @@
+package com.example.zoodelille.data.repository.quiz.answer.local;
+
+import com.example.zoodelille.data.db.ProjectDatabase;
+import com.example.zoodelille.data.entity.quiz.answer.AnswerEntity;
+
+import java.util.List;
+
+import io.reactivex.Completable;
+import io.reactivex.Flowable;
+import io.reactivex.Single;
+
+public class AnswerLocalDataSource {
+    private final ProjectDatabase projectDatabase;
+
+    public AnswerLocalDataSource(ProjectDatabase database) {
+        this.projectDatabase = database;
+    }
+
+    public Completable addAnswer(AnswerEntity answerEntity){
+        return projectDatabase.answerDao().addAnswer(answerEntity);
+    }
+
+    public Completable addAllAnswer(List<AnswerEntity> answerEntity){
+        return projectDatabase.answerDao().addAllAnswer(answerEntity);
+    }
+
+    public Completable deleteAnswerWithId(int id){
+        return projectDatabase.answerDao().deleteAnswerWithId(id);
+    }
+
+    public Single<AnswerEntity> getAnswerEntity(int id){
+        return projectDatabase.answerDao().getAnswerEntity(id);
+    }
+
+    public Flowable<List<AnswerEntity>> getAllAnswer(){
+        return projectDatabase.answerDao().getAllAnswer();
+    }
+
+    public Flowable<List<AnswerEntity>> getAllAnswerWithQuestionId(int id){
+        return projectDatabase.answerDao().getAllAnswerWithQuestionId(id);
+    }
+}
---
+++ ZooDeLille/app/src/main/java/com/example/zoodelille/data/repository/quiz/answer/mapper/AnswerToAnswerEntity.java
@@ -0,0 +1,26 @@
+package com.example.zoodelille.data.repository.quiz.answer.mapper;
+
+import com.example.zoodelille.data.api.object.quiz.answer.Answer;
+import com.example.zoodelille.data.entity.quiz.answer.AnswerEntity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class AnswerToAnswerEntity {
+    public static AnswerEntity map(Answer answer, int question_id){
+        AnswerEntity answerEntity = new AnswerEntity();
+        answerEntity.setAnswer_id(answer.getId());
+        answerEntity.setAnswer(answer.getAnswer());
+        answerEntity.setUrl_picture(answer.getUrl_picture());
+        answerEntity.setQuestion_id(question_id);
+        return answerEntity;
+    }
+
+    public static List<AnswerEntity> map(List<Answer> answers, int question_id){
+        List<AnswerEntity> answerEntities = new ArrayList<>();
+        for(Answer answer : answers){
+            answerEntities.add(map(answer,question_id));
+        }
+        return answerEntities;
+    }
+}
