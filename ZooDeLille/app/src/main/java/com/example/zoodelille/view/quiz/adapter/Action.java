68fa19eead2eba1c26f79b70e08a56c31626bdcb
GregPhi <gregoire.philippot@laposte.net>
2021-02-09 00:08:16 +0100

    create : package adapter action to perform when user want to play list adapter -> quizzes display in fragment_quiz package item -> item in list of object to display and / or to play

---
+++ ZooDeLille/app/src/main/java/com/example/zoodelille/view/quiz/adapter/Action.java
@@ -0,0 +1,7 @@
+package com.example.zoodelille.view.quiz.adapter;
+
+import com.example.zoodelille.view.quiz.adapter.item.QuizItemViewModel;
+
+public interface Action {
+    void playAQuiz(QuizItemViewModel quizItemViewModel);
+}
