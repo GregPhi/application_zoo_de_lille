b6fecf860b38559cc81ca4871b17a68859b338a1
GregPhi <gregoire.philippot@laposte.net>
2021-02-09 16:26:52 +0100

    fix : create a map list quiz entity to list quiz item

--- ZooDeLille/app/src/main/java/com/example/zoodelille/view/quiz/mapper/QuizEntityToQuizItemViewModel.java
+++ ZooDeLille/app/src/main/java/com/example/zoodelille/view/quiz/mapper/QuizEntityToQuizItemViewModel.java
@@ -3,9 +3,21 @@ package com.example.zoodelille.view.quiz.mapper;
 import com.example.zoodelille.data.entity.quiz.QuizEntity;
 import com.example.zoodelille.view.quiz.adapter.item.QuizItemViewModel;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class QuizEntityToQuizItemViewModel {
     public QuizItemViewModel map(QuizEntity quizEntity){
         QuizItemViewModel quizItemViewModel = new QuizItemViewModel();
+        quizItemViewModel.setName(quizEntity.getQuiz_name());
+        return quizItemViewModel;
+    }
+
+    public List<QuizItemViewModel> map(List<QuizEntity> quizEntities){
+        List<QuizItemViewModel> quizItemViewModel = new ArrayList<>();
+        for(QuizEntity quizEntity : quizEntities){
+            quizItemViewModel.add(map(quizEntity));
+        }
         return quizItemViewModel;
     }
 
