34269055ebe5df51280747b8bf59c8daebdcc12b
GregPhi <gregoire.philippot@laposte.net>
2021-02-09 14:03:05 +0100

    fix : view model info zooIsOpen -> call the zooIsOpen reposioty and return if it's open ? getInfoEntityMutableLiveData -> call the getInfoEntity repository to show on the information activity the zoo's information

--- ZooDeLille/app/src/main/java/com/example/zoodelille/view/model/InfoViewModel.java
+++ ZooDeLille/app/src/main/java/com/example/zoodelille/view/model/InfoViewModel.java
@@ -1,5 +1,7 @@
 package com.example.zoodelille.view.model;
 
+import android.util.Log;
+
 import com.example.zoodelille.data.entity.info.InfoEntity;
 import com.example.zoodelille.data.repository.info.InfoRepository;
 
@@ -9,6 +11,7 @@ import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.disposables.CompositeDisposable;
+import io.reactivex.observers.DisposableSingleObserver;
 import io.reactivex.schedulers.Schedulers;
 import io.reactivex.subscribers.ResourceSubscriber;
 
@@ -17,6 +20,7 @@ public class InfoViewModel extends ViewModel {
     private final CompositeDisposable compositeDisposable;
 
     private final MutableLiveData<InfoEntity> infoEntityMutableLiveData = new MutableLiveData<>();
+    private final MutableLiveData<Boolean> zooIsOpen = new MutableLiveData<>();
 
     public InfoViewModel(InfoRepository infoRepository) {
         this.infoRepository = infoRepository;
@@ -24,7 +28,6 @@ public class InfoViewModel extends ViewModel {
     }
 
     public MutableLiveData<InfoEntity> getInfoEntityMutableLiveData(){
-        compositeDisposable.clear();
         compositeDisposable.add(infoRepository.getInfoEntity()
                 .subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread())
@@ -44,7 +47,23 @@ public class InfoViewModel extends ViewModel {
         return infoEntityMutableLiveData;
     }
 
-    public boolean zooIsOpen(){
-        return infoRepository.zooIsOpen();
+    public MutableLiveData<Boolean> zooIsOpen(){
+        compositeDisposable.clear();
+        compositeDisposable.add(infoRepository.zooIsOpen()
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribeWith(new DisposableSingleObserver<Boolean>() {
+                    @Override
+                    public void onSuccess(Boolean open) {
+                        System.out.println("OPEN? :"+open);
+                        zooIsOpen.setValue(open);
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+                        Log.e("TAG_ZOO_IS_OPEN?","error -> ",e);
+                    }
+                }));
+        return zooIsOpen;
     }
 }
