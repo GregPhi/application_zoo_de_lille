839a59d66de1e66216498b4515faabc18c61f58f
GregPhi <gregoire.philippot@laposte.net>
2021-02-09 14:00:03 +0100

    fix ; method zoo is open ? get annual closure and exceptional opening check if today (day, month and year) is not in condition

--- ZooDeLille/app/src/main/java/com/example/zoodelille/data/repository/info/InfoRepository.java
+++ ZooDeLille/app/src/main/java/com/example/zoodelille/data/repository/info/InfoRepository.java
@@ -1,10 +1,13 @@
 package com.example.zoodelille.data.repository.info;
 
+import android.annotation.SuppressLint;
+
 import com.example.zoodelille.data.api.object.info.Info;
 import com.example.zoodelille.data.entity.info.InfoEntity;
 import com.example.zoodelille.data.repository.info.local.InfoLocalDataSource;
 import com.example.zoodelille.data.repository.info.remote.InfoRemoteDataSource;
 
+import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.GregorianCalendar;
 import java.util.List;
@@ -12,6 +15,7 @@ import java.util.List;
 import io.reactivex.Completable;
 import io.reactivex.Flowable;
 import io.reactivex.Single;
+import io.reactivex.functions.Function3;
 
 public class InfoRepository {
     private final InfoLocalDataSource infoLocalDataSource;
@@ -38,42 +42,54 @@ public class InfoRepository {
         return infoRemoteDataSource.getAllInfos();
     }
 
-    public boolean zooIsOpen(){
-        boolean open = true;
+    public Single<Boolean> zooIsOpen(){
+        Single<String> getAnnualClosureOldYear = infoLocalDataSource.getAnnualClosureOldYear();
+        Single<String> getAnnualClosureNewYear = infoLocalDataSource.getAnnualClosureNewYear();
+        Single<String> getExceptionalOpening = infoLocalDataSource.getExceptionalOpening();
 
-        int whatDayIsToday = new GregorianCalendar().get(GregorianCalendar.DAY_OF_MONTH);
-        int whatNameDayIsToday = new GregorianCalendar().get(GregorianCalendar.DAY_OF_WEEK);
-        int whatMonthIsToday = new GregorianCalendar().get(GregorianCalendar.MONTH);
-        int whatYearIsToday = new GregorianCalendar().get(GregorianCalendar.YEAR);
-
-        String getAnnualClosureOldYear = infoLocalDataSource.getAnnualClosureOldYear();
-        String getAnnualClosureNewYear = infoLocalDataSource.getAnnualClosureNewYear();
-        String getExceptionalOpening = infoLocalDataSource.getExceptionalOpening();
-        String[] except_open = getExceptionalOpening.split("\n");
-
-        Date oldYear = new Date(getAnnualClosureOldYear);
-        Date newYear = new Date(getAnnualClosureNewYear);
-
-        if((whatNameDayIsToday == GregorianCalendar.THURSDAY) || (whatMonthIsToday == GregorianCalendar.JANUARY || whatMonthIsToday == GregorianCalendar.DECEMBER)){
-            open = false;
-        }else{
-            if(oldYear.getYear() == whatYearIsToday){
-                if(whatMonthIsToday == oldYear.getMonth() && whatDayIsToday > oldYear.getDay()){
-                    open = false;
-                }
-            }
-            if(newYear.getYear() == whatYearIsToday){
-                if(whatMonthIsToday == newYear.getMonth() && whatDayIsToday > newYear.getDay()){
-                    open = false;
-                }
-            }
-            for(String e : except_open){
-                Date e_op = new Date(e);
-                if(whatMonthIsToday == e_op.getMonth() && whatDayIsToday > e_op.getDay()){
-                    open = true;
-                }
-            }
-        }
-        return open;
+        return Single.zip(getAnnualClosureOldYear, getAnnualClosureNewYear, getExceptionalOpening,
+                new Function3<String, String, String, Boolean>() {
+                    @Override
+                    public Boolean apply(String closeO, String closeN, String exceptO) throws Exception {
+                        boolean open = true;
+                        int whatDayIsToday = new GregorianCalendar().get(GregorianCalendar.DAY_OF_MONTH);
+                        int whatNameDayIsToday = new GregorianCalendar().get(GregorianCalendar.DAY_OF_WEEK);
+                        int whatMonthIsToday = new GregorianCalendar().get(GregorianCalendar.MONTH);
+                        int whatYearIsToday = new GregorianCalendar().get(GregorianCalendar.YEAR);
+                        @SuppressLint("SimpleDateFormat") SimpleDateFormat formater = new SimpleDateFormat("dd MMMM yyyy");
+                        Date oldYear = formater.parse(closeO);
+                        Date newYear = formater.parse(closeN);
+                        String[] except_open = exceptO.split("\n");
+                        if(whatNameDayIsToday == 3){
+                            return false;
+                        }else if(whatMonthIsToday == GregorianCalendar.JANUARY || whatMonthIsToday == GregorianCalendar.DECEMBER){
+                            return false;
+                        }else{
+                            if(oldYear!=null){
+                                if((oldYear.getYear()-100) == (whatYearIsToday-2000)){
+                                    if(whatMonthIsToday == oldYear.getMonth() && whatDayIsToday > oldYear.getDay()){
+                                        open = false;
+                                    }
+                                }
+                            }
+                            if(newYear!=null){
+                                if((newYear.getYear()-100) == (whatYearIsToday-2000)){
+                                    if(whatMonthIsToday == newYear.getMonth() && whatDayIsToday > newYear.getDay()){
+                                        open = false;
+                                    }
+                                }
+                            }
+                            for(String e : except_open){
+                                Date e_op = formater.parse(e);
+                                if(e_op!=null){
+                                    if(whatMonthIsToday == e_op.getMonth() && whatDayIsToday > e_op.getDay()){
+                                        open = true;
+                                    }
+                                }
+                            }
+                        }
+                        return open;
+                    }
+                });
     }
 }
