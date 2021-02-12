22f68b0aa9bf7f10a246db31cdbd2538a5613e5a
GregPhi <gregoire.philippot@laposte.net>
2021-02-12 12:08:05 +0100

    fix : quiz repository add method to get a quiz to play fix method to get all quiz to display

--- ZooDeLille/app/src/main/java/com/example/zoodelille/data/repository/quiz/QuizRepository.java
+++ ZooDeLille/app/src/main/java/com/example/zoodelille/data/repository/quiz/QuizRepository.java
@@ -5,18 +5,20 @@ import com.example.zoodelille.data.entity.quiz.QuizEntity;
 import com.example.zoodelille.data.entity.quiz.answer.AnswerEntity;
 import com.example.zoodelille.data.entity.quiz.question.QuestionEntity;
 import com.example.zoodelille.data.repository.quiz.answer.local.AnswerLocalDataSource;
-import com.example.zoodelille.data.repository.quiz.answer.mapper.AnswerToAnswerEntity;
 import com.example.zoodelille.data.repository.quiz.local.QuizLocalDataSource;
-import com.example.zoodelille.data.repository.quiz.mapper.QuizToQuizEntity;
 import com.example.zoodelille.data.repository.quiz.question.local.QuestionLocalDataSource;
-import com.example.zoodelille.data.repository.quiz.question.mapper.QuestionToQuestionEntity;
 import com.example.zoodelille.data.repository.quiz.remote.QuizRemoteDataSource;
+import com.example.zoodelille.view.quiz.adapter.item.QuestionItemViewModel;
+import com.example.zoodelille.view.quiz.adapter.item.QuizItemViewModel;
+import com.example.zoodelille.view.quiz.mapper.QuizEntityToQuizItemViewModel;
 
 import java.util.List;
 
 import io.reactivex.Completable;
 import io.reactivex.Flowable;
 import io.reactivex.Single;
+import io.reactivex.functions.BiFunction;
+import io.reactivex.functions.Function;
 
 public class QuizRepository {
     private final QuizLocalDataSource quizLocalDataSource;
@@ -24,9 +26,7 @@ public class QuizRepository {
     private final QuestionLocalDataSource questionLocalDataSource;
     private final AnswerLocalDataSource answerLocalDataSource;
 
-    private final QuizToQuizEntity quizToQuizEntity = new QuizToQuizEntity();
-    private final QuestionToQuestionEntity questionToQuestionEntity = new QuestionToQuestionEntity();
-    private final AnswerToAnswerEntity answerToAnswerEntity = new AnswerToAnswerEntity();
+    private final QuizEntityToQuizItemViewModel quizEntityToQuizItemViewModel = new QuizEntityToQuizItemViewModel();
 
     public QuizRepository(QuizLocalDataSource quizLocalDataSource, QuizRemoteDataSource quizRemoteDataSource, QuestionLocalDataSource questionLocalDataSource, AnswerLocalDataSource answerLocalDataSource) {
         this.quizLocalDataSource = quizLocalDataSource;
@@ -39,6 +39,11 @@ public class QuizRepository {
         return quizLocalDataSource.addQuiz(quizEntity);
     }
 
+    public Completable addQuizMake(QuizItemViewModel quizItemViewModel){
+        return quizLocalDataSource.addQuiz(quizEntityToQuizItemViewModel.reverse(quizItemViewModel));
+    }
+
+
     public Completable addAllQuiz(List<QuizEntity> quizEntity){
         return quizLocalDataSource.addAllQuiz(quizEntity);
     }
@@ -47,15 +52,21 @@ public class QuizRepository {
         return quizLocalDataSource.deleteQuizWithId(id);
     }
 
-    public Single<QuizEntity> getQuizEntity(int id){
+    public Flowable<QuizEntity> getQuizEntity(int id){
         return quizLocalDataSource.getQuizEntity(id);
     }
 
-    public Flowable<List<QuizEntity>> getAllQuiz(){
-        return quizLocalDataSource.getAllQuiz();
+    public Flowable<List<QuizItemViewModel>> getAllQuiz(){
+        return quizLocalDataSource.getAllQuiz()
+                .map(new Function<List<QuizEntity>, List<QuizItemViewModel>>() {
+                    @Override
+                    public List<QuizItemViewModel> apply(List<QuizEntity> quizEntities) throws Exception {
+                        return quizEntityToQuizItemViewModel.map(quizEntities);
+                    }
+                });
     }
 
-    public Flowable<List<Integer>> getAllMakeQuiz(){
+    public Single<List<Integer>> getAllMakeQuiz(){
         return quizLocalDataSource.getAllMakeQuiz();
     }
 
@@ -110,4 +121,57 @@ public class QuizRepository {
     public Flowable<List<AnswerEntity>> getAllAnswerWithQuestionId(int id){
         return answerLocalDataSource.getAllAnswerWithQuestionId(id);
     }
+
+    public Flowable<QuizItemViewModel> getAQuiz(int id){
+        final Flowable<List<QuestionItemViewModel>> questionItemViewModels = questionLocalDataSource.getAllQuestionWithQuizId(id)
+                .map(new Function<List<QuestionEntity>, List<QuestionItemViewModel>>() {
+                    @Override
+                    public List<QuestionItemViewModel> apply(List<QuestionEntity> questionEntities) throws Exception {
+                        return quizEntityToQuizItemViewModel.mapQuestions(questionEntities);
+                    }
+                }).zipWith(answerLocalDataSource.getAllAnswer(), new BiFunction<List<QuestionItemViewModel>, List<AnswerEntity>, List<QuestionItemViewModel>>() {
+                    @Override
+                    public List<QuestionItemViewModel> apply(List<QuestionItemViewModel> questionItemViewModels, List<AnswerEntity> answerEntities) throws Exception {
+                        for(QuestionItemViewModel questionItemViewModel : questionItemViewModels){
+                            for(AnswerEntity answerEntity : answerEntities){
+                                if(questionItemViewModel.getId() == answerEntity.getQuestionid()){
+                                    questionItemViewModel.addAnswer(quizEntityToQuizItemViewModel.mapAnswer(answerEntity));
+                                }
+                            }
+                        }
+                        return questionItemViewModels;
+                    }
+                });
+
+        final Flowable<QuizItemViewModel> quizItemViewModel = quizLocalDataSource.getQuizEntity(id)
+                .map(new Function<QuizEntity, QuizItemViewModel>() {
+                    @Override
+                    public QuizItemViewModel apply(QuizEntity quizEntity) throws Exception {
+                        return quizEntityToQuizItemViewModel.map(quizEntity);
+                    }
+                });
+
+        return Flowable.zip(quizItemViewModel, questionItemViewModels, new BiFunction<QuizItemViewModel, List<QuestionItemViewModel>, QuizItemViewModel>() {
+            @Override
+            public QuizItemViewModel apply(QuizItemViewModel quizItemViewModel, List<QuestionItemViewModel> questionItemViewModels) throws Exception {
+                quizItemViewModel.setQuestions(questionItemViewModels);
+                return quizItemViewModel;
+            }
+        });
+    }
+
+/*
+    public Flowable<List<QuizItemViewModel>> getAllQuiz(){
+        final Flowable<List<QuestionEntity>> questionEntities = questionLocalDataSource.getAllQuestion();
+        final Flowable<List<AnswerEntity>> answerEntities = answerLocalDataSource.getAllAnswer();
+        final Flowable<List<QuizEntity>> quizEntities = quizLocalDataSource.getAllQuiz();
+        Flowable<List<QuizItemViewModel>> quiz = Flowable.zip(quizEntities, questionEntities, answerEntities, new Function3<List<QuizEntity>, List<QuestionEntity>, List<AnswerEntity>, List<QuizItemViewModel>>() {
+            @Override
+            public List<QuizItemViewModel> apply(List<QuizEntity> quizEntities, List<QuestionEntity> questionEntities, List<AnswerEntity> answerEntities) throws Exception {
+                return quizEntityToQuizItemViewModel.mapAll(quizEntities,questionEntities,answerEntities);
+            }
+        });
+        return quiz;
+    }
+ */
 }
